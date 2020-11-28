0b8471e257e12767927207f8b753fe56092972aa
Maria Martinez <>
2020-11-28 11:44:58 +0100

    modificacion crearcuenta y añado js de asignaturas

--- crearcuenta.php
+++ crearcuenta.php
@@ -15,6 +15,11 @@
       border: 2px outset black;
       min-width:200px;
   }
+
+  #contenedor_asig{
+      margin-left: 35px;
+  }
+
   h2{
     text-align: center;
   }
@@ -47,27 +52,16 @@
 </head>
 
 <script src="./scripts/pasarPaginaCuenta.js"></script>
+<script src="./scripts/asignaturasCurso.js" defer></script>
 
 <body onload="mostrarTab(0)">
   <?php
   include 'cabecera.php';
   ?>
 
-<form id="formulario">
-<h2>Introduzca los datos</h2>
-<div class="contenedor">
-    <div class="elem">
-    <label for="user">Usuario</label><br>
-    <input type="text" id="user" name="user"><br>
-    </div>
-    <div class="elem">
-    <label for="passwd">Contrase&ntildea:</label><br>
-    <input type="password" id="passwd" name="passwd"><br>
-    </div>
-    <div class="elem">
-    <label for="reppasswd">Repetir Contrase&ntildea:</label><br>
-    <input type="password" id="reppasswd" name="reppasswd"><br>
-    </div>
+  <form id="formulario" method="post" enctype="multipart/form-data">
+  <h2>Introduzca los datos</h2>
+  <div class="contenedor">
     <div class="elem">
     <label for="nombre">Nombre </label><br>
     <input type="text" id="nombre" name="nombre"><br>
@@ -76,67 +70,44 @@
     <label for="apell">Apellidos </label><br>
     <input type="text" id="apell" name="apell"><br>
     </div>
-    <div class="elem">
-    <label for="genero">Genero:</label>
-    <select id="genero" name="genero">
-        <option value="hombre" selected>Hombre</option>
-        <option value="mujer">Mujer</option>
-        <option value="nobinario">No binario </option>
-    </select><br>
-    </div>
-    <div class="elem">
-    <label for="fnacimiento">Fecha de nacimiento</label><br>
-    <input type="date" id="fnacimiento" name="fnacimiento">
-    </div>
-    <div class="elem">
-    <label for="telefono">Numero de telefono</label><br>
-    <input type="text" id="telefono" name="telefono"><br>
-    </div>
-</div>
-<div class="contenedor">
-  <div class="elem">
-  <label for="correouva">Correo de la UVA</label><br>
-  <input type="text" id="correouva" name="correouva"><br>
+     <div class="elem">
+     <label for="correouva">Correo de la UVA</label><br>
+     <input type="text" id="correouva" name="correouva"><br>
+      </div>
+      <div class="elem">
+      <label for="passwd">Contrase&ntildea:</label><br>
+      <input type="password" id="passwd" name="passwd"><br>
+      </div>
+      <div class="elem">
+      <label for="reppasswd">Repetir Contrase&ntildea:</label><br>
+      <input type="password" id="reppasswd" name="reppasswd"><br>
+      </div>
+      <div class="elem">
+      <label for="dni">DNI</label><br>
+      <input type="text" id="dni" name="dni"><br>
+      </div>
+      <div class="elem">
+      <label for="fnacimiento">Fecha de nacimiento</label><br>
+      <input type="date" id="fnacimiento" name="fnacimiento">
+      </div>
+      <div class="elem">
+      <label for="telefono">Numero de telefono</label><br>
+      <input type="text" id="telefono" name="telefono"><br>
+      </div>
   </div>
-    <div class="elem">
-    <label for="grado">Grado</label><br>
-    <input type="text" id="grado" name="grado"><br>
-  </div>
-    <div class="elem">
-    <label for="curso">Curso:</label>
-    <select id="curso" name="curso">
-        <option value="primero" selected>1º</option>
-        <option value="segundo">2º</option>
-        <option value="tercero">3º</option>
-        <option value="cuarto">4º</option>
-        <option value="quinto">5º</option>
-    </select>
-    </div>
-    <div class="elem">
-    <label for="metodo">Metodo de docencia</label>
-    <select id="metodo" name="metodo">
-        <option value="presencial" selected>Presencial</option>
-        <option value="nopresencial">No presencial</option>
-        <option value="mixta">Mixta</option>
-    </select>
-    </div>
-    <div class="elem">
-    <label for="horario">Horario</label>
-    <select id="horario" name="horario">
-        <option value="manana" selected>Ma&ntildeana</option>
-        <option value="tarde">Tarde</option>
-        <option value="ambos">Ambos</option>
-    </select><br>
-    </div>
-    <div class="elem">
-    <label for="aulas">Aulas atendidas*</label><br>
-    <input type="aulas" id="aulas" name="aulas"><br>
+  <div class="contenedor">
+      <div class="elem">
+      <label for="curso">Curso:</label>
+      <select id="curso" name="curso" onchange="mostrarAsignaturas(this);">
+     		  <option value="" selected> </option>
+          <option value="1">1º</option>
+          <option value="2">2º</option>
+          <option value="3">3º</option>
+          <option value="4">4º</option>
+      </select>
+      </div>
+      <div class="elem" id="contenedor_asig"></div><br>
   </div><br>
-      <div class="elem" style="color:red;">*Poner un 0 en caso de no presencial</div><br>
-    <div class="elem">
-    <label><input type="checkbox" name="terms" checked="true">He leído y acepto los <u>términos y condiciones de uso</u></label><br>
-  </div><br>
-</div><br>
 <div>
   <div id="botonpeq" style="float:right;">
     <button type="button" id="prevBtn" onclick="siguienteAnterior(-1)" style="font-family:sans-serif;font-size:14px;">Anterior</button>
