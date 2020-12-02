b6a92c26e3251884f930491a695d4de5b6b989a4
Marco <markolas14@gmail.com>
2020-12-02 14:23:20 +0100

    creado sql para inicializar tablas y n_matriculados toma valor 0 por defecto

--- sql/tabla_asignaturas.sql
+++ sql/tabla_asignaturas.sql
@@ -1,8 +1,9 @@
 create table asignaturas(
 codigo int unsigned not null auto_increment primary key,
+nombre char(255) not null,
 aula char(50) not null,
 lab char(50),
-profesor char(100) not null,
+profesor char(255) not null,
 n_matriculados int unsigned,
 curso int unsigned,
 img_a varchar(500),
