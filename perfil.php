93f3128ce774200ee68d3aef8004b45542cc56d9
Rúben Serrano <RubenSerrano@lab8>
2020-12-05 13:10:34 +0100

    perfil.php con parte de Test

--- perfil.php
+++ perfil.php
@@ -27,13 +27,14 @@
   ?>
   <div class = "contenedorPrincipal">
     <?php #comprobamos que podamos acceder a la base de datos
-    $bd	=	mysqli_connect("localhost",	"root",	"root",	"bd");
+    $bd	=	mysqli_connect("localhost",	"root",	"");
+	  mysqli_select_db($bd,	"bd");
     if	(mysqli_connect_errno())	{
         echo	"Error:	"	.	mysqli_connect_error()	.	".	<br>";
         exit();
     }
     $query	=	"SELECT * FROM	tabla_personas";
-    $resultado	=	mysqli_query(bd,	query);
+    $resultado	=	mysqli_query($bd,	$query);
     for ($i = 0; $i < mysqli_num_rows($resultado);$i++){
       $fila = mysqli_fetch_array($resultado);
       if ($fila['email'] == $GLOBALS["arrayProyecto(0)"]){ #aqui comprobaremos que se escoge a la persona correcta
@@ -55,10 +56,10 @@
     echo "".$fila['telefono']."";
     ?></br>
   </div>
-  <div class="contenedorPrincipal"> #aqui podemos listar las asignaturas
+  <div class="contenedorPrincipal">
     <?php
     $query	=	"SELECT * FROM	tabla_per_asig";
-    $resultado	=	mysqli_query(bd,	query);
+    $resultado	=	mysqli_query($bd,	$query);
     for ($i = 0; $i < mysqli_num_rows($resultado);$i++){
       $fila2 = mysqli_fetch_array($resultado);
       if($fila2['email'] == $fila['email']){
@@ -66,7 +67,7 @@
       }
     }
     $query	=	"SELECT * FROM	tabla_asignaturas";
-    $resultado	=	mysqli_query(bd,	query);
+    $resultado	=	mysqli_query($bd,	$query);
     for ($i = 0; $i < mysqli_num_rows($resultado);$i++){
       $fila = mysqli_fetch_array($resultado);
       for($k = 0; $k<count($arrayAsig);$k++){
@@ -81,8 +82,28 @@
     ?>
   </div>
   <div class="contenedorPrincipal">
-    <?php #aqui vamos a listar la tabla de los test e indicar cuando se ha hecho el test
-
+    <?php #primero comprobaremos que no este ya confinado, luego listaremos las fechas en un calendario
+    $query	=	"SELECT * FROM	tabla_asignaturas";
+    $resultado	=	mysqli_query($bd,	$query);
+    $id = 0;
+    for($i = 0; $i < mysqli_num_rows($resultado); $i++){
+        $fila = mysqli_fetch_array($resultado); #aqui cojo la ultima entrada de la base de datos de una misma persona (por si ha estado mas de una vez confinado)
+        if(($id < $fila['identificador'])&&($fila['email']==$GLOBALS["arrayProyecto(0)"])){
+          $id = $filas['identificador'];
+          $fila2 = $fila;
+        }
+    }
+    $timestamp = time();
+    $fecha_actual = getdate($timestamp); #OJO, MIRAR LO DE LAS FECHAS, PARA COGERLAS BIEN
+    if(($fila2 == NULL)||($fila2['fecha_desconf'] < $fecha_actual)){ #Esto indica que no esta confinado, o que no ha estado confinado en ningun momento
+      echo 'Por favor, indique si ha dado positivo o le han hecho el test, pero ha dado negativo y esta en cuarentena; </br></br>
+      <input type = "radio" id = "Si" name = "positivo" value="1">
+      <label for "Si"> He dado positivo o estoy confinado. </label></br></br>
+      <input type = "submit" value = "Enviar">';
+    }
+    if(($fila2 != NULL)&&($fila2['fecha_desconf'] > $fecha_actual)){ #OJO COMPROBAR QUE COJO BIEN LAS FECHAS, O SI LAS COJO!
+      
+    }
 
     ?>
   </div>
